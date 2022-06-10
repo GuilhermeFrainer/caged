6709e18a49792f1bc0d96bd54c436f2fcdc9bb86
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2022-06-09 23:44:13 -0300

    Incomplete date setter function

--- data.py
+++ data.py
@@ -1,4 +1,26 @@
+import datetime
+
+
 # Class to handle caged data and ensure a value is connected to its date
 class Data:
-    def __init__(date, value):
-        ...
+    def __init__(self, date, value):
+        self.date = date
+        self.value = value
+
+    @property
+    def date(self):
+        return self._date
+
+    # Expects either date already
+    @date.setter
+    def date(self, date):
+        
+        if isinstance(date, datetime.datetime):
+            self._date = date
+
+        else:
+            months = {'Janeiro': 1, 'Fevereiro': 2, 'Março': 3, 'Abril': 4, 'Maio': 5, 'Junho': 6, 'Julho': 7, 'Agosto': 8, 'Setembro': 9, 'Outubro': 10, 'Novembro': 11, 'Dezembro': 12}
+            month, year = date.split("/")
+            month = months[month]
+            date = datetime.date.fromisoformat(f'{year}-{month:02d}-01')
+            self._date = date
